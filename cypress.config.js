f709581288407e5a318df8d0a8aa5a3d2612209f
Joan León <joan.leon@gmail.com>
2022-09-20 09:33:18 +0200

    Add excludeSpecPattern

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://example.cypress.io',
+    excludeSpecPattern: ['**/1-getting-started/**'], // '**/2-advanced-examples/**'
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
