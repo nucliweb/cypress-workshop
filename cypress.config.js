3c7507f12484489b4a47002312ec8356b5c03a97
Joan León <joan.leon@gmail.com>
2022-09-21 00:55:48 +0200

    Add support to WebKit

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '2couhy',
+  experimentalWebKitSupport: true,
   e2e: {
     includeShadowDom: true,
     excludeSpecPattern: ['**/1-getting-started/**', '**/2-advanced-examples/**'],
