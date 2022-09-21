9a1d580695fc53947f3788a705bbdf4d58e4b017
Joan León <joan.leon@gmail.com>
2022-09-21 18:41:57 +0200

    DEfine 0 to default command delay

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,7 +26,7 @@
 
 // Set CYPRESS_COMMAND_DELAY above zero for demoing to stakeholders,
 // E.g. CYPRESS_COMMAND_DELAY=1000 node_modules/.bin/cypress open
-const COMMAND_DELAY = Cypress.env('COMMAND_DELAY') || 2000;
+const COMMAND_DELAY = Cypress.env('COMMAND_DELAY') || 0;
 if (COMMAND_DELAY > 0) {
     for (const command of ['visit', 'click', 'trigger', 'type', 'clear', 'reload', 'contains', 'viewport']) {
         Cypress.Commands.overwrite(command, (originalFn, ...args) => {
